77782f43e95bc99834e1595a17efacce7fbf8469
KevinL <mastahkevin@gmail.com>
2019-05-27 19:11:43 -0700

    Added dotenv package and updated connection.js to use process.env... for connection.

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// dotenv file to contain db configuration
+require('dotenv').config();
+
+// Set up MySQL connection.
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'burgers_db'
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;

f0baf591ce879bdf73b436189ed2da9a512fc85e
KevinL <mastahkevin@gmail.com>
2019-06-04 08:02:48 -0700

    Updating for Heroku

--- config/connection.js
+++ config/connection.js
@@ -4,22 +4,17 @@ require('dotenv').config();
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: 'burgers_db'
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'burgers_db'
+  });
+}
 
 // Export connection for our ORM to use.
 module.exports = connection;
